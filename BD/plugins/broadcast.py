e660b57d0934a02044de14e9142f2b3fb83a98c4
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-10-28 17:56:51 +0530

    Update broadcast.py

--- BD/plugins/broadcast.py
+++ BD/plugins/broadcast.py
@@ -2,8 +2,8 @@ from . import *
 from telethon.sync import events
 from BD import DEVS
 
-@tbot.on(events.NewMessage(pattern="[/!]bcast")
-async def lmoa(e):
+@tbot.on(events.NewMessage(pattern="[/!]bcast"))
+async def lmoa(event):
    if event.sender.id in DEVS:
       try:
          for y in [-1001176306346, -1001686303934]
