184fcbb2daba86c89732e86f0f6742772c26879d
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-10-28 21:36:50 +0530

    Update broadcast.py

--- BD/plugins/broadcast.py
+++ BD/plugins/broadcast.py
@@ -8,6 +8,6 @@ async def lmoa(event):
          for y in [-1001176306346, -1001686303934]:
              x = await event.get_reply_message()
              await tbot.send_message(y,x)
-             await event.reply('**Successfully Braodcasted**')
+             await event.send_message(event.chat_id, '**Successfully Braodcasted**', reply_to=event)
       except Exception as lol:
              await event.client.send_message(-1001686303934, f'Error {lol}')
