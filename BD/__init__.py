ad79f1ce13f4b31e5d295c0f1a02d4d79bc9853c
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-10-28 18:01:12 +0530

    Update __init__.py

--- BD/__init__.py
+++ BD/__init__.py
@@ -6,6 +6,6 @@ import time
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.INFO)
 
-DRVS= {int(x) for x in os.environ.get("DEVS", "").split()}
+DEVS= {int(x) for x in os.environ.get("DEVS", "").split()}
 
 tbot = TelegramClient('botto', API_KEY, API_HASH).start(bot_token=TOKEN)
