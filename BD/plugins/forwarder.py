54f9c7873758af864250cbbb06945a044c84694c
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-10-28 21:55:52 +0530

    Update forwarder.py

--- BD/plugins/forwarder.py
+++ BD/plugins/forwarder.py
@@ -1,9 +1,9 @@
-from BD import tbot
+from BD import tbot, GC_ID, CH_ID
 from telethon.sync import events
 
 @tbot.on(events.NewMessage(incoming=True))
 async def x(e):
- if e.chat.id ==1381212488 and e.sender.id ==1354488699:
+ if e.chat.id in GC_ID and e.sender.id in CH_ID:
   try:
     for x in [-1001176306346, -1001443691244]:
         await e.forward_to(x)
