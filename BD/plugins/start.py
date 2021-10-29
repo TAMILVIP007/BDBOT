31f0f951d6c948aae373c0fd7049a6eb14f97d58
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-10-29 22:23:49 +0530

    update start.py

--- BD/plugins/start.py
+++ BD/plugins/start.py
@@ -1,12 +1,12 @@
 from BD import tbot
 from telethon.sync import events
-from BD import SUPPORT, CHANNEL
+from BD import SUPPORT, CHANNEL, START_IMG
 from telethon import Button
 
 
 @tbot.on(events.NewMessage(pattern="[/!]start"))
 async def lol(event):
-  await tbot.send_message(event.chat_id, f'**Hey {(event.sender.first_name)}.\n I am a forwader bot made for [Channel](t.me/{CHANNEL}).**', file="https://telegra.ph//file/62aa918d3d321651d166d.jpg", buttons=[[Button.url('Support',f't.me/{SUPPORT}'), Button.url('Channel',f't.me/{CHANNEL}')], [Button.inline('Help',data="fk")]])
+  await tbot.send_message(event.chat_id, f'**Hey {(event.sender.first_name)}.\n I am a forwader bot made for [Channel](t.me/{CHANNEL}).**', file=START_IMG, buttons=[[Button.url('Support',f't.me/{SUPPORT}'), Button.url('Channel',f't.me/{CHANNEL}')], [Button.inline('Help',data="fk")]],reply_to=event)
   
 @tbot.on(events.CallbackQuery(pattern=r"fk"))
 async def lmao(event):
@@ -14,4 +14,4 @@ async def lmao(event):
 
 @tbot.on(events.CallbackQuery(pattern=r"lmao"))
 async def nigga(event):
-   await event.edit(f'__Hey {(event.sender.first_name)}.\n I am a forwader bot made for [Channel](t.me/{CHANNEL})__.', file="https://telegra.ph//file/62aa918d3d321651d166d.jpg", buttons=[[Button.url('Support',f't.me/{SUPPORT}'), Button.url('Channel',f't.me/{CHANNEL}')],[Button.inline('Help',data="fk")]])
+   await event.edit(f'__Hey {(event.sender.first_name)}.\n I am a forwader bot made for [Channel](t.me/{CHANNEL})__.', file=START_IMG, buttons=[[Button.url('Support',f't.me/{SUPPORT}'), Button.url('Channel',f't.me/{CHANNEL}')],[Button.inline('Help',data="fk")]])
