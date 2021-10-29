17062a6f1d82fe2ab8ff8564f02a60d9d11d6ea9
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-10-29 21:12:58 +0530

    update start.py

--- BD/plugins/start.py
+++ BD/plugins/start.py
@@ -6,8 +6,7 @@ from telethon import Button
 
 @tbot.on(events.NewMessage(pattern="[/!]start"))
 async def lol(event):
-  await tbot.send_message(event.chat_id, f'__Hey {(event.sender.first_name)}.\n I am a forwader bot made for {CHANNEL}__.', file="https://telegra.ph//file/62aa918d3d321651d166d.jpg", buttons=Button.url('Support',f't.me/{SUPPORT}'),Button.url('Channel',f't.me/{CHANNEL}'), Button.inline('Help',data="fk"))
-  
+  await tbot.send_message(event.chat_id, f'__Hey {(event.sender.first_name)}.\n I am a forwader bot made for {CHANNEL}__.')
 @tbot.on(events.CallbackQuery(pattern=r"fk"))
 async def lmao(event):
   await event.edit('Lol Nothing to describe on help\n\n\n **DEV CMDS**\n `/bcast` To broadcast a msg', button=Button.inline('Back', data='lmao'))
