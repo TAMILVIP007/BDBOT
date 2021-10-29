3a7f219d28f9dfdaebf152ef96ebd055aa32d82e
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-10-29 21:58:44 +0530

    update __init__.py

--- BD/__init__.py
+++ BD/__init__.py
@@ -9,10 +9,11 @@ logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s'
                     level=logging.INFO)
 
 DEVS= {int(x) for x in os.environ.get("DEVS", "").split()}
-GC_ID = os.environ.get("GC_ID", None)
-CH_ID = os.environ.get("CH_ID", None)
+GC_ID = {int(y) for y in os.environ.get("GC_ID", "").split()}
+CH_ID = {int(z) for z in os.environ.get("GC_ID", "").split()}
 LOG_SPAM = os.environ.get("LOG_SPAM", None)
 SUPPORT = os.environ.get("SUPPORT", None)
 CHANNEL = os.environ.get("CHANNEL", None)
+START_IMG = os.environ.get("START_IMG", None)
 
 tbot = TelegramClient('botto', API_KEY, API_HASH).start(bot_token=TOKEN)
