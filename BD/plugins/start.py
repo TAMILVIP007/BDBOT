4cb9e453c9bcf378ac39300a213ac4e78639add2
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-10-29 21:34:05 +0530

    update start.py

--- BD/plugins/start.py
+++ BD/plugins/start.py
@@ -6,4 +6,4 @@ from telethon import Button
 
 @tbot.on(events.NewMessage(pattern="[/!]start"))
 async def lol(event):
-  await tbot.send_message(event.chat_id, f'__Hey {(event.sender.first_name)}.\n I am a forwader bot made for [Channel]](t.me/{CHANNEL})__.', file="https://telegra.ph//file/62aa918d3d321651d166d.jpg", buttons=[Button.url('Support',f't.me/{SUPPORT}'), Button.url('Channel',f't.me/{CHANNEL}'), Button.inline('Help',data="fk")])
+  await tbot.send_message(event.chat_id, f'__Hey {(event.sender.first_name)}.\n I am a forwader bot made for [Channel](t.me/{CHANNEL})__.', file="https://telegra.ph//file/62aa918d3d321651d166d.jpg", buttons=[Button.url('Support',f't.me/{SUPPORT}'), Button.url('Channel',f't.me/{CHANNEL}')], [Button.inline('Help',data="fk")])
