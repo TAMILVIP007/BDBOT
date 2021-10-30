8f051c928134eb4dbe945fee9cf88e5ea6601934
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-10-30 20:27:50 +0530

    Update db.py

--- BD/plugins/db.py
+++ BD/plugins/db.py
@@ -1,7 +1,8 @@
 from pymongo import MongoClient
 from BD import DB_URL, BOT_ID, tbot, LOG_SPAM
 from telethon.sync import events
-db = MongoClient(DB_URL)["botto"]
+db = MongoClient(DB_URL)
+db = db["botto"]
 
 @tbot.on(events.ChatAction(func=lambda e: e.user_added and e.user_id == BOT_ID))
 async def ok(e):
