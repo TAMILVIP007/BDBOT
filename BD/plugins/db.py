9ef573e25129ee349d42373361771a957cbd6a15
TAMILVIP007 <79161058+TAMILVIP007@users.noreply.github.com>
2021-10-30 10:57:57 +0530

    Update and rename db to db.py

--- BD/plugins/db.py
+++ BD/plugins/db.py
@@ -5,4 +5,4 @@ from telethon.sync import events
 
 @tbot.on(events.ChatAction(func=lambda e: e.user_added and e.user_id == BOT_ID))
 async def ok(e):
-     await tbot.send_message(LOG_SPAM, 'I've been added to (event.chat.id)')
+     await tbot.send_message(LOG_SPAM, 'I've been added to (e.chat.id)')
